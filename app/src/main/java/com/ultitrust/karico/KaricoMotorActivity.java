5ed0ca4805e0f964098bacf8b8a3110e6070cb2e
Gh0stM0de <=jdotcole233@gmail.com>
2019-08-23 23:22:51 +0000

    chooser version 1.0 done

--- app/src/main/java/com/ultitrust/karico/KaricoMotorActivity.java
+++ app/src/main/java/com/ultitrust/karico/KaricoMotorActivity.java
@@ -1,14 +1,17 @@
 package com.ultitrust.karico;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class KaricoMotorActivity extends AppCompatActivity {
 
     private ImageButton music_dismiss, music_file_chooser;
+    private final int REQUEST_CODE = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,21 @@ public class KaricoMotorActivity extends AppCompatActivity {
         music_file_chooser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent open_chooser = new Intent(Intent.ACTION_GET_CONTENT);
-                open_chooser.addCategory(Intent.CATEGORY_OPENABLE);
-                open_chooser.setType("*/*");
-                startActivityForResult(open_chooser, 42);
+                Intent open_chooser = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                open_chooser.addCategory(Intent.CATEGORY_DEFAULT);
+                startActivityForResult(Intent.createChooser(open_chooser, "Choose Directory"), REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                Log.i("Karico", data.getData().toString());
+                break;
+
+        }
     }
 }
