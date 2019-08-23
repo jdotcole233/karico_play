4f893f1b555cf2127fb7338a6c18a800b8ae1555
Gh0stM0de <=jdotcole233@gmail.com>
2019-08-23 22:36:02 +0000

    ui version 1.0 done

--- app/src/main/java/com/ultitrust/karico/KaricoMenuActivity.java
+++ app/src/main/java/com/ultitrust/karico/KaricoMenuActivity.java
@@ -1,13 +1,43 @@
 package com.ultitrust.karico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class KaricoMenuActivity extends AppCompatActivity {
+
+    private Button motor_activity_btn, speech_activity_btn, other_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_karico_menu);
+        motor_activity_btn = findViewById(R.id.motoractivitybutton);
+        speech_activity_btn = findViewById(R.id.speechbutton);
+        other_btn = findViewById(R.id.otherbutton);
+
+        speech_activity_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(KaricoMenuActivity.this, "Try again later!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        other_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(KaricoMenuActivity.this, "Try again later", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        motor_activity_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(KaricoMenuActivity.this, KaricoMotorActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/ultitrust/karico/KaricoMotorActivity.java
+++ app/src/main/java/com/ultitrust/karico/KaricoMotorActivity.java
@@ -1,13 +1,37 @@
 package com.ultitrust.karico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class KaricoMotorActivity extends AppCompatActivity {
+
+    private ImageButton music_dismiss, music_file_chooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_karico_motor);
+        music_dismiss = findViewById(R.id.musicdismissbtn);
+        music_file_chooser = findViewById(R.id.musicfilechooser);
+
+        music_dismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        music_file_chooser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open_chooser = new Intent(Intent.ACTION_GET_CONTENT);
+                open_chooser.addCategory(Intent.CATEGORY_OPENABLE);
+                open_chooser.setType("*/*");
+                startActivityForResult(open_chooser, 42);
+            }
+        });
     }
 }
