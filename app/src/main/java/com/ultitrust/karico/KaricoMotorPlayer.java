62db24d6a3ebed0bd9aaa2411aa4df9ed0954423
Gh0stM0de <=jdotcole233@gmail.com>
2019-08-23 21:24:36 +0000

    first commit

---
+++ app/src/main/java/com/ultitrust/karico/KaricoMotorPlayer.java
@@ -0,0 +1,13 @@
+package com.ultitrust.karico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KaricoMotorPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karico_motor_player);
+    }
+}
